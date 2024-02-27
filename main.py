ee6d5f95292da1c16503e928423f5d5604e833f9
Stephen Horvath <steve.horvath@hpe.com>
2024-02-26 18:21:43 -0800

    init

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import csv
+import tkinter as tk
 
 
 class Programconfig:
@@ -16,8 +17,32 @@ class Programconfig:
             print("Error on open file for read")
 
 
+class App(tk.Tk):
+    def __init__(self,Title,Geometry):
+        super().__init__()
+        self.title(Title)
+        self.geometry(Geometry)
+        self.label = tk.Label(self, text="Hello")
+        self.label.pack()
+        self.button = tk.Button(self, text='Click Me')
+        self.button['command'] = self.button_clicked
+        self.button.pack()
+
+    def button_clicked(self):
+        T = tk.Text(self, height=500, width=200)
+        T.pack()
+        self.label.pack()
+
+
+
+
+
+
 if __name__ == '__main__':
     print("Program Config")
     cfg = Programconfig("config.csv")
     cfg.readconfig()
+
+    app = App('myTitle','300x50')
+    app.mainloop()
 
