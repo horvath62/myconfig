69fc631feacc269e3bdf204c480e036bd3870141
Stephen Horvath <steve.horvath@hpe.com>
2024-05-24 14:16:46 -0700

    init

--- main.py
+++ main.py
@@ -72,47 +72,69 @@ class App(tk.Tk):
         self.newcfg = {}
 
         self.check1 = tk.Checkbutton(self)
-        self.check1.grid(row=0, column=0)
-        self.labelfilename = tk.Label(self, text="Default Config file:"+cfgfile)
-        self.labelfilename.grid(row = 0, column = 0, columnspan= 2)
+        self.check1.grid(row=1,column=0)
+        self.check2 = tk.Checkbutton(self)
+        self.check2.grid(row=2, column=0)
+        self.check3 = tk.Checkbutton(self)
+        self.check3.grid(row=3, column=0)
+
+
+
+        self.labelfilename = tk.Label(self, text="Config file:")
+        self.labelfilename.grid(row = 0, column = 2)
+
+        self.buttonsave = tk.Button(self, text='SAVE')
+        self.buttonsave['command'] = self.buttonsave_clicked
+        self.buttonsave.grid(row = 0, column = 1)
+        self.buttonread = tk.Button(self, text='READ')
+        self.buttonread['command'] = self.buttonread_clicked
+        self.buttonread.grid(row = 0, column = 0)
+
+
+
+
+
+        self.label1 = tk.Label(self, text="Check1",anchor="w")
+        self.label1.grid(row=1, column=1)
+
+
+
+
+
         self.textboxcfgfile= tk.Text(self, height=1, width=20)
-        self.textboxcfgfile.grid(row=0, column=2)
+        self.textboxcfgfile.grid(row=0, column=3)
         self.textboxcfgfile.insert(tk.END, self.cfgfile)
 
         self.button10 = tk.Button(self, text='GO', pady=10)
         self.button10['command'] = self.button10_clicked
-        self.button10.grid(row = 1, column = 0, pady = 10)
+        self.button10.grid(row = 4, column = 0, pady = 10)
 
         self.button11 = tk.Button(self, text='Button11', pady=10)
         self.button11['command'] = self.button11_clicked
-        self.button11.grid(row = 1, column = 1)
+        self.button11.grid(row = 4, column = 1)
 
         self.button12 = tk.Button(self, text='Button12', pady=10)
         self.button12['command'] = self.button12_clicked
-        self.button12.grid(row = 1, column = 2)
+        self.button12.grid(row = 4, column = 2)
 
         self.button13 = tk.Button(self, text='Button13', pady=10)
         self.button13['command'] = self.button13_clicked
-        self.button13.grid(row = 1, column = 3)
-
-        self.button20 = tk.Button(self, text='button20', pady=10)
-        self.button20['command'] = self.button20_clicked
-        self.button20.grid(row = 2, column = 0, pady = 10)
+        self.button13.grid(row = 4, column = 3)
 
         self.button21 = tk.Button(self, text='Button21', pady=10)
         self.button21['command'] = self.button21_clicked
-        self.button21.grid(row = 2, column = 1)
+        self.button21.grid(row = 5, column = 1)
 
         self.button22 = tk.Button(self, text='Button22', pady=10)
         self.button22['command'] = self.button22_clicked
-        self.button22.grid(row = 2, column = 2)
+        self.button22.grid(row = 5, column = 2)
 
         self.button23 = tk.Button(self, text='Button23', pady=10)
         self.button23['command'] = self.button23_clicked
-        self.button23.grid(row = 2, column = 3)
+        self.button23.grid(row = 5, column = 3)
 
         # Set the offset for the starting row of the dynamic text input fields below the buttons.
-        self.rowoffset = 4
+        self.rowoffset = 7
 
     def button10_clicked(self):
         pass
@@ -124,9 +146,6 @@ class App(tk.Tk):
         pass
 
     def button13_clicked(self):
-        pass
-
-    def button20_clicked(self):
         pass
 
     def button21_clicked(self):
@@ -148,8 +167,6 @@ class App(tk.Tk):
             self.label_index[index].destroy()
             # print("destroyed:",index)
             pass
-        self.buttonread.destroy()
-        self.buttonsave.destroy()
         self.buttoninsert.destroy()
 
         self.textbox_key = {}
@@ -166,24 +183,19 @@ class App(tk.Tk):
         self.buttoninsert = tk.Button(self, text='INSERT')
         self.buttoninsert['command'] = self.buttoninsert_clicked
         self.buttoninsert.grid(row = self.rowoffset+len(self.textbox_key), column = 0, pady=5)
-        self.buttonsave = tk.Button(self, text='SAVE')
-        self.buttonsave['command'] = self.buttonsave_clicked
-        self.buttonsave.grid(row = 0, column = 4)
-        self.buttonread = tk.Button(self, text='READ')
-        self.buttonread['command'] = self.buttonread_clicked
-        self.buttonread.grid(row = 0, column = 3)
+
 
     def add_cfgtextbox(self,index,key,value):
         # print(">>",index, key, value)
         label = tk.Label(self, text=index)
-        label.grid(row=index+self.rowoffset, column=0)
+        label.grid(row=index+self.rowoffset, column=1)
         self.label_index[index] = label
         textbox = tk.Text(self, height=1, width=20)
-        textbox.grid(row=index+self.rowoffset, column=1)
+        textbox.grid(row=index+self.rowoffset, column=2)
         textbox.insert(tk.END, key)
         self.textbox_key[index] = textbox
         textbox = tk.Text(self, height=1, width=20)
-        textbox.grid(row=index+self.rowoffset, column=2)
+        textbox.grid(row=index+self.rowoffset, column=3)
         textbox.insert(tk.END, value)
         self.textbox_value[index] = textbox
         # print("create:",index,key,value)
@@ -262,7 +274,7 @@ if __name__ == '__main__':
 
     # print(cfg.cfgfile)
 
-    app = App("Program Config", cfg.cfgfile, cfg.cfgdata, '500x300')
+    app = App("Program Config", cfg.cfgfile, cfg.cfgdata, '600x400')
     app.create_textboxes()
 
     app.mainloop()
